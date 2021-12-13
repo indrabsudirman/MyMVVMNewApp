8c2e7f5c47ad04c3bbdbee3381a941e939ead464
indrabsudirman <indrabsudirman@gmail.com>
2021-12-13 16:52:07 +0700

    Part 8, but not working while test. Still error

--- app/src/main/java/id/indrasudirman/mymvvmnewapp/ui/NewsViewModel.kt
+++ app/src/main/java/id/indrasudirman/mymvvmnewapp/ui/NewsViewModel.kt
@@ -16,12 +16,22 @@ class NewsViewModel(
     val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
 
+    init {
+        getBreakingNews("us")
+    }
+
     fun getBreakingNews(countryCode: String) = viewModelScope.launch {
         breakingNews.postValue(Resource.Loading())
         val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
     }
 
     private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
-
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
     }
 }
--- app/src/main/java/id/indrasudirman/mymvvmnewapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/id/indrasudirman/mymvvmnewapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,19 +1,79 @@
 package id.indrasudirman.mymvvmnewapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.indrasudirman.mymvvmnewapp.R
+import id.indrasudirman.mymvvmnewapp.adapters.NewsAdapter
+import id.indrasudirman.mymvvmnewapp.databinding.ActivityNewsBinding.inflate
+import id.indrasudirman.mymvvmnewapp.databinding.FragmentBreakingNewsBinding
 import id.indrasudirman.mymvvmnewapp.ui.NewsActivity
 import id.indrasudirman.mymvvmnewapp.ui.NewsViewModel
+import id.indrasudirman.mymvvmnewapp.util.Resource
 
 class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    // View Binding
+    private lateinit var fragmentBreakingNewsBinding: FragmentBreakingNewsBinding
+
+    val TAG = "BreakingNewsFragment"
+
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         viewModel = (activity as NewsActivity).viewModel
+
+        setupRecyclerView()
+
+        viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "an error occurred: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
     }
+
+    private fun hideProgressBar() {
+        fragmentBreakingNewsBinding.paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        fragmentBreakingNewsBinding.paginationProgressBar.visibility = View.VISIBLE
+    }
+
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapter()
+        fragmentBreakingNewsBinding.recyclerViewBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+
 }
